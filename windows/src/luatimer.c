3c73dc755e9560abfa5adc1f03af0760e66d65e1
Bruno Silvestre <brunoos@inf.ufg.br>
2007-10-25 12:05:28 +0000

    Code identation.

--- windows/src/luatimer.c
+++ windows/src/luatimer.c
@@ -10,32 +10,32 @@
 
 static int lt_time(lua_State *L)
 {
-	FILETIME filetime;
-    LARGE_INTEGER tmp;
-    double now;
+  FILETIME filetime;
+  LARGE_INTEGER tmp;
+  double now;
 
-	/* FILETIME: Contains a 64-bit value representing the number of
-	   100-nanosecond intervals since January 1, 1601 (UTC). */
-	GetSystemTimeAsFileTime(&filetime);
-    tmp.LowPart  = filetime.dwLowDateTime;
-    tmp.HighPart = filetime.dwHighDateTime;
+  /* FILETIME: Contains a 64-bit value representing the number of
+     100-nanosecond intervals since January 1, 1601 (UTC). */
+  GetSystemTimeAsFileTime(&filetime);
+  tmp.LowPart  = filetime.dwLowDateTime;
+  tmp.HighPart = filetime.dwHighDateTime;
 	
-	/* Convert to microseconds since Epoch, like gettimeofday */
-    now = (double)((tmp.QuadPart - LT_EPOCH) / 10LL);
-	/* Convert to seconds */
-	now /= 1E6f;
-	lua_pushnumber(L, now);
+  /* Convert to microseconds since Epoch, like gettimeofday */
+  now = (double)((tmp.QuadPart - LT_EPOCH) / 10LL);
+  /* Convert to seconds */
+  now /= 1E6f;
+  lua_pushnumber(L, now);
 
-	return 1;
+  return 1;
 }
 
 struct luaL_Reg funcs[] = {
-	{"time", lt_time},
-	{NULL, NULL}
+  {"time", lt_time},
+  {NULL, NULL}
 };
 
 DllExport int luaopen_luatimer_os(lua_State *L)
 {
-	luaL_register(L, "luatimer.os", funcs);
-	return 1;
+  luaL_register(L, "luatimer.os", funcs);
+  return 1;
 }
